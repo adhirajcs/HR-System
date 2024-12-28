cc7878d2572d20f0149a0d7471150bafe0221af7
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-12-28 15:21:42 +0000

    Added utility functions -> CRUD ops for PM by HR

--- backend/main/utils.py
+++ backend/main/utils.py
@@ -2,6 +2,36 @@ from datetime import datetime
 from .models import User, Employee, ProjectManager, HR
 
 
+# Utility function to create HR user
+def create_hr_user(first_name, last_name, email, branch, birthday, password):
+    """
+    Utility to create an HR user and associate it with an HR profile.
+    """
+    # Generate a username by combining first name and branch
+    username = f"{first_name.lower()}_{branch.lower().replace(' ', '_')}"  # Replace spaces with underscores in branch name
+
+    # Create the user
+    user = User.objects.create_user(
+        username=username,
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password,
+        role="HR",
+    )
+
+    # Create the HR profile
+    HR.objects.create(
+        username=user,
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        branch=branch,
+        birthday=birthday,
+    )
+    return user
+
+
 # Utility function to create employee user data
 def create_employee_user(
     first_name,
@@ -92,7 +122,7 @@ def update_employee(
 def delete_employee(username):
     """
     Utility to delete Employee data by username.
-    
+
     Returns:
         bool: True if the employee is successfully deleted, False if the employee does not exist.
     """
@@ -114,7 +144,7 @@ def create_project_manager_user(
     Utility to create a Project Manager user and associate it with a Project Manager profile.
     """
     # Generate a username by combining first name and last name
-    username = f"{first_name.lower()}_{last_name.lower()}"
+    username = f"{first_name.lower()}_{datetime.now().strftime('%y%m%d%H%M%S')}"
 
     # Create the user
     user = User.objects.create_user(
@@ -138,31 +168,64 @@ def create_project_manager_user(
     return user
 
 
-# Utility function to create HR user
-def create_hr_user(first_name, last_name, email, branch, birthday, password):
+# Utility function to update project manager user data
+def update_project_manager(
+    username,
+    first_name=None,
+    last_name=None,
+    email=None,
+    phone_number=None,
+    department=None,
+    birthday=None,
+):
     """
-    Utility to create an HR user and associate it with an HR profile.
+    Utility to update Project manager data by username and update the associated User as well.
     """
-    # Generate a username by combining first name and branch
-    username = f"{first_name.lower()}_{branch.lower().replace(' ', '_')}"  # Replace spaces with underscores in branch name
+    try:
+        user = User.objects.get(username=username, role="PROJECT_MANAGER")
+        project_manager_profile = ProjectManager.objects.get(username=user)
+        if first_name:
+            user.first_name = first_name
+            project_manager_profile.first_name = first_name
+        if last_name:
+            user.last_name = last_name
+            project_manager_profile.last_name = last_name
+        if email:
+            user.email = email
+            project_manager_profile.email = email
+        if phone_number:
+            project_manager_profile.phone_number = phone_number
+        if department:
+            project_manager_profile.department = department
+        if birthday:
+            project_manager_profile.birthday = birthday
+        
+        user.save()
+        project_manager_profile.save()
+        return project_manager_profile
+    except (User.DoesNotExist, project_manager_profile.DoesNotExist):
+        return None
+    
 
-    # Create the user
-    user = User.objects.create_user(
-        username=username,
-        first_name=first_name,
-        last_name=last_name,
-        email=email,
-        password=password,
-        role="HR",
-    )
+# Utility function to delete project manager data
+def delete_project_manager(username):
+    """
+    Utility to delete Project Manager data by username.
+    
+    Returns:
+        bool: True if the project manager is successfully deleted, False if the project manager does not exist.
+    """
+    try:
+        user = User.objects.get(username=username, role="PROJECT_MANAGER")
+        employee = Employee.objects.get(username=user)
+        employee.delete()
+        user.delete()
+        return True
+    except (User.DoesNotExist, Employee.DoesNotExist):
+        return False
+    
+# Utility function to get all HRs
 
-    # Create the HR profile
-    HR.objects.create(
-        username=user,
-        first_name=first_name,
-        last_name=last_name,
-        email=email,
-        branch=branch,
-        birthday=birthday,
-    )
-    return user
+# Utility function to get all employees
+
+# Utility function to get all project managers
